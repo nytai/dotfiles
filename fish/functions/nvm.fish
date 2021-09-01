6b7ca77692a903efd249a85495f84d1eccab8b63
Tai Dupree <tdupreetan@gmail.com>
2021-09-01 14:24:28 -0700

    fix nvm

--- fish/functions/nvm.fish
+++ fish/functions/nvm.fish
@@ -1,102 +1,52 @@
-#? NVM wrapper. Félix Saparelli. Public Domain
-#> https://github.com/passcod/nvm-fish-wrapper
-#v 1.2.2
+function nvm-fast
+	set -q NVM_DIR ; or set -l NVM_DIR ~/.nvm
+	set -l brigand_nvm_fish_path $NVM_DIR/versions/node
+	if test (count $argv[1]) -lt 1
+		echo 'nvm-fast: at least one argument is required'
+	end
 
-function nvm_set
-  if test (count $argv) -gt 1
-    #echo set: k: $argv[1] v: $argv[2..-1]
-    set -gx $argv[1] $argv[2..-1]
-  else
-    #echo unset: k: $argv[1]
-    set -egx $argv[1]
-  end
-end
+	set -l command $argv[1]
+	if test $command = 'use'
+		set target_version "unknown"
+		if test (count $argv) -eq 1
+			if test -f .nvmrc
+				set target_version (cat .nvmrc)
+			else
+				echo "Error: No version provided and no .nvmrc found"
+				return
+			end
+		else
+			set target_version $argv[2]
+		end
+		set -l target_version $target_version
 
-function nvm_split_env
-  set k (echo $argv | cut -d\= -f1)
-  set v (echo $argv | cut -d\= -f2-)
-  echo $k
-  echo $v
-end
+		set -l matched_version (bash -c "source $NVM_DIR/nvm.sh --no-use; nvm_version $target_version")
 
-function nvm_find_paths
-  echo $argv | grep -oE '[^:]+' | grep -w '.nvm'
-end
-
-function nvm_set_path
-  set k $argv[1]
-  set r $argv[2..-1]
-
-  set newpath
-  for o in $$k
-    if echo $o | grep -qvw '.nvm'
-      set newpath $newpath $o
-    end
-  end
-
-  set p (nvm_find_paths $r | head -n1)
-  set newpath $p $newpath
-  nvm_set $k $newpath
-end
-
-function nvm_mod_env
-  set tmpnew $tmpdir/newenv
-
-  bash -c "source ~/.nvm/nvm.sh && source $tmpold && nvm $argv && export status=\$? && env > $tmpnew && exit \$status"
-
-  set nvmstat $status
-  if test $nvmstat -gt 0
-    return $nvmstat
-  end
-
-  for e in (cat $tmpnew)
-    set p (nvm_split_env $e)
-
-    if test (echo $p[1] | cut -d_ -f1) = NVM
-      if test (count $p) -lt 2
-        nvm_set $p[1] ''
-        continue
-      end
-
-      nvm_set $p[1] $p[2..-1]
-      continue
-    end
-
-    if test $p[1] = PATH
-      nvm_set_path PATH $p[2..-1]
-    else if test $p[1] = MANPATH
-      set -l t (echo $p[2..-1] | cut -sd\: -f2-)
-      if test '' = "$t"
-        # We're assuming that if there's only one path
-        # in the MANPATH, we should append the default
-        # value. That may be wrong in some edge-cases.
-        set -l m $p[2..-1]:(manpath -g)
-        nvm_set MANPATH $m
-      else
-        nvm_set MANPATH $p[2..-1]
-      end
-    end
-  end
-
-  return $nvmstat
+		if test -z $matched_version -o $matched_version = 'N/A'
+			echo "No version installed for $target_version, run nvm install $target_version"
+			echo "Installed versions: "
+			for file in $brigand_nvm_fish_path/v*
+				echo ' -' $file
+			end
+		else
+			set -l new_path (string match -ev "$brigand_nvm_fish_path" $PATH)
+			if test $matched_version != 'system'
+				set new_path $brigand_nvm_fish_path/$matched_version/bin $new_path
+			end
+			set -gx PATH $new_path
+			set -gx NVM_BIN (which node)
+		end
+	else
+		bash -c "source $NVM_DIR/nvm.sh --no-use; nvm $argv"
+		set -gx NVM_BIN (which node)
+	end
 end
 
 function nvm
-  set -g tmpdir (mktemp -d 2>/dev/null; or mktemp -d -t 'nvm-wrapper') # Linux || OS X
-  set -g tmpold $tmpdir/oldenv
-  env | grep -E '^((NVM|NODE)_|(MAN)?PATH=)' > $tmpold
-
-  set -l arg1 $argv[1]
-  if echo $arg1 | grep -qE '^(use|install|deactivate)$'
-    nvm_mod_env $argv
-    set s $status
-  else if test '$arg1' = 'unload'
-    functions -e (functions | grep -E '^nvm(_|$)')
-  else
-    bash -c "source ~/.nvm/nvm.sh && source $tmpold && nvm $argv"
-    set s $status
-  end
-
-  command rm -r $tmpdir
-  return $s
+	switch "$FISH_VERSION"
+		case 2.0.0 2.1.0 2.1.1 2.1.2 2.2.0 2.2b1 2.3.0 2.3.1 2.3b1 2.3b2 2.4.0 2.4b1 2.5.0 2.5b1
+			echo "You need fish 2.6.0 or higher to use fast-nvm-fish." 1>&2
+			return 1
+	end
+	nvm-fast $argv
 end
